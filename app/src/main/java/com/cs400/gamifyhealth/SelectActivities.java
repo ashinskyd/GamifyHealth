fc901fb020d2b7a2652243d7ddca8e83e7d7dd79
David Ashinsky <ashinskyd@carleton.edu>
2014-11-10 20:11:42 -0600

    Fixed some layout stuff.

--- app/src/main/java/com/cs400/gamifyhealth/SelectActivities.java
+++ app/src/main/java/com/cs400/gamifyhealth/SelectActivities.java
@@ -27,61 +27,38 @@ public class SelectActivities extends Activity {
     private ListView activitiesListView;
     private CheckBoxAdapter checkBoxAdapter;
     public ArrayList<ActivityModel> activityItems;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_activities);
         activitiesListView = (ListView) findViewById(R.id.activityListView);
+
         activityItems = new ArrayList<ActivityModel>();
+
         activityItems.add(0,new ActivityModel("Running",false));
-        activityItems.add(0,new ActivityModel("Swimming",false));
+        activityItems.add(1,new ActivityModel("Swimming",false));
+        activityItems.add(2,new ActivityModel("Crunches",false));
+
         CheckBoxAdapter adapter = new CheckBoxAdapter(getApplicationContext(),R.layout.checkbox_layout,activityItems);
         activitiesListView.setAdapter(adapter);
-        activitiesListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d("TAG", "Position: "+i);
-            }
-        });
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.select_activities, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
     private class CheckBoxAdapter extends ArrayAdapter<ActivityModel>{
-        private ArrayList<ActivityModel> activityList;
+       // private ArrayList<ActivityModel> activityList;
         private Context context;
 
         public CheckBoxAdapter(Context context, int textViewResourceId, ArrayList<ActivityModel> activityList){
             super(context, textViewResourceId,activityList);
-            this.activityList = new ArrayList<ActivityModel>();
-            this.activityList.addAll(activityList);
+           // this.activityList = new ArrayList<ActivityModel>();
+            //this.activityList.addAll(activityList);
             this.context = context;
         }
 
-        private class ViewHolder{
-            CheckBox checkBox;
-        }
+
 
         public View getView(final int position, View convertView, ViewGroup parent){
-            ViewHolder holder = null;
             LayoutInflater inflater = LayoutInflater.from(context);
             convertView = inflater.inflate(R.layout.checkbox_layout, parent, false);
             final CheckBox cb = (CheckBox) convertView.findViewById(R.id.checkBox1);
@@ -89,17 +66,17 @@ public class SelectActivities extends Activity {
             cb.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    activityList.get(position).setChecked(cb.isChecked());
-                    for(int i=0;i<activityList.size();i++){
-                        Log.d("TAG","Item: "+activityList.get(i).getName());
-                        Log.d("TAG","Value: "+activityList.get(i).isChecked());
+                    activityItems.get(position).setChecked(cb.isChecked());
+                    for(int i=0;i<activityItems.size();i++){
+                        Log.d("TAG","Item: "+activityItems.get(i).getName());
+                        Log.d("TAG","Value: "+activityItems.get(i).isChecked());
                     }
                 }
             });
             return convertView;
         }
         }
-    public class ActivityModel{
+    private class ActivityModel{
         private String name;
         private boolean isChecked;
 
