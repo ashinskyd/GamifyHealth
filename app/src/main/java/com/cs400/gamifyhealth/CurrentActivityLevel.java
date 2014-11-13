218270f2bad030725f51cdcbfe073c9673d0e5a2
David Ashinsky <ashinskyd@live.com>
2014-11-13 12:58:04 -0600

    Added the seekbar stuff, need to finish the profile setup

--- app/src/main/java/com/cs400/gamifyhealth/CurrentActivityLevel.java
+++ app/src/main/java/com/cs400/gamifyhealth/CurrentActivityLevel.java
@@ -1,36 +1,80 @@
 package com.cs400.gamifyhealth;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class CurrentActivityLevel extends Activity {
-
+    private LinearLayout container;
+    private SeekBar slider;
+    private SharedPreferences sharedPrefs;
+    private Map<Integer,String> idMap;
+    private Map<Integer,TextView> idMap2;
+    private TextView title;
+    private TextView progress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_current_activity_level);
-    }
+        idMap = new HashMap<Integer,String>();
+        idMap2 = new HashMap<Integer,TextView>();
+        sharedPrefs = getApplicationContext().getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+        String[] activities = sharedPrefs.getString("ACTIVITIES","").split(",");
+        container = (LinearLayout) findViewById(R.id.container);
 
+        for(int i=0;i<activities.length;i++){
+            slider = new SeekBar(getApplicationContext());
+            title = new TextView(getApplicationContext());
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.current_activity_level, menu);
-        return true;
-    }
+            progress = new TextView(getApplicationContext());
+            progress.setGravity(Gravity.CENTER);
+            progress.setText(Integer.toString(slider.getProgress()));
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+            title.setText(activities[i]);
+            idMap.put(i, activities[i]);
+            idMap2.put(i,progress);
+            slider.setId(i);
+            slider.setProgressDrawable(getResources().getDrawable(R.drawable.progress_drawable));
+            slider.setThumb(getResources().getDrawable(R.drawable.thumb_drawable));
+
+            slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+                @Override
+                public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                    Log.d("TAG","ID: "+idMap.get(seekBar.getId()));
+                    Log.d("TAG","Value Set: "+seekBar.getProgress());
+                    TextView v = idMap2.get(seekBar.getId());//.setText(seekBar.getProgress());
+                    v.setText(Integer.toString(seekBar.getProgress()));
+                }
+
+                @Override
+                public void onStartTrackingTouch(SeekBar seekBar) {
+                }
+                @Override
+                public void onStopTrackingTouch(SeekBar seekBar) {
+                }
+
+            });
+            container.addView(title);
+            container.addView(slider);
+            container.addView(progress);
         }
-        return super.onOptionsItemSelected(item);
+
+
     }
+
+
+
+
 }
--- app/src/main/java/com/cs400/gamifyhealth/SelectActivities.java
+++ app/src/main/java/com/cs400/gamifyhealth/SelectActivities.java
@@ -3,6 +3,7 @@ package com.cs400.gamifyhealth;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -19,9 +20,11 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Dictionary;
 import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.Map;
 
 
@@ -30,6 +33,7 @@ public class SelectActivities extends Activity {
     private CheckBoxAdapter checkBoxAdapter;
     private ArrayList<ActivityModel> activityItems;
     private Button continueButton;
+    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,11 +41,36 @@ public class SelectActivities extends Activity {
         setContentView(R.layout.activity_select_activities);
         activitiesListView = (ListView) findViewById(R.id.activityListView);
         continueButton = (Button) findViewById(R.id.continueButton2);
+        context = getApplicationContext();
         continueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getApplicationContext(),CurrentActivityLevel.class );
-                //TODO: Send to arraylist to shared prefs
+                SharedPreferences sharedPref = context.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                StringBuilder sb = new StringBuilder();
+                List<String> repArray;
+                List<String> dtaArray;
+                repArray = Arrays.asList(getString(R.string.activity_types_REP).split(","));
+                dtaArray = Arrays.asList(getString(R.string.activity_types_DTA).split(","));
+                for (int j = 0; j < activityItems.size(); j++) {
+                    if (activityItems.get(j).getIsChecked()) {
+                        if(repArray.contains(activityItems.get(j).getName())){
+                            sb.append(activityItems.get(j).getName()).append("_REP").append(",");
+
+                        }else if(dtaArray.contains(activityItems.get(j).getName())){
+                            //TODO: Need more specific DTA info based on slection
+                            sb.append(activityItems.get(j).getName()).append("_DTA").append(",");
+
+                        }else{
+                            sb.append(activityItems.get(j).getName()).append("_TIM").append(",");
+
+                        }
+                    }
+                }
+                editor.putString("ACTIVITIES",sb.toString());
+                editor.commit();
+
                 i.setFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
                 i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
                 startActivity(i);
@@ -49,14 +78,15 @@ public class SelectActivities extends Activity {
             }
         });
         activityItems = new ArrayList<ActivityModel>();
-        activityItems.add(0,new ActivityModel("Running",false));
+        activityItems.add(0, new ActivityModel("Running", false));
         activityItems.add(1,new ActivityModel("Swimming",false));
         activityItems.add(2,new ActivityModel("Crunches",false));
-        activityItems.add(3,new ActivityModel("Jogging",false));
-        activityItems.add(4,new ActivityModel("Jogging",false));
-        activityItems.add(5,new ActivityModel("Jogging",false));
-        activityItems.add(6,new ActivityModel("Jogging",false));
-        activityItems.add(7,new ActivityModel("Jogging",false));
+        activityItems.add(3, new ActivityModel("Cycling", false));
+        activityItems.add(4,new ActivityModel("Pull-Ups",false));
+        activityItems.add(5,new ActivityModel("Dips",false));
+        activityItems.add(6,new ActivityModel("Push-Ups",false));
+        activityItems.add(7,new ActivityModel("Walking",false));
+        activityItems.add(7,new ActivityModel("Squats",false));
         CheckBoxAdapter adapter = new CheckBoxAdapter(getApplicationContext(),R.layout.checkbox_layout,activityItems);
         activitiesListView.setAdapter(adapter);
     }
@@ -80,7 +110,7 @@ public class SelectActivities extends Activity {
                     activityItems.get(position).setChecked(cb.isChecked());
                     for(int i=0;i<activityItems.size();i++){
                         Log.d("TAG","Item: "+activityItems.get(i).getName());
-                        Log.d("TAG","Value: "+activityItems.get(i).isChecked());
+                        Log.d("TAG","Value: "+activityItems.get(i).getIsChecked());
                     }
                 }
             });
@@ -100,7 +130,7 @@ public class SelectActivities extends Activity {
             return this.name;
         }
 
-        public boolean isChecked(){
+        public boolean getIsChecked(){
             return this.isChecked;
         }
 
